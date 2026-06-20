f312a045e42a7c815a33176c89226fdb74288e8a
vishalkale9 <vishalkale1367@gmail.com>
2026-06-20 16:26:50 +0530

    db config

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
